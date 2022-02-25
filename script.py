dce646d953f8e744e9c8b9bf89295210a4f5ba72
Jaival Patel <jaival@Jaivals-MacBook-Air.local>
2022-02-25 11:28:16 -0500

    fetching

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from platformdirs import user_runtime_dir
+
 from blockchain import Blockchain
 import time
 from datetime import datetime
@@ -61,7 +61,7 @@ class Article:
             name = key 
             article = value 
         
-        transaction = {'name': name, 'article_name': [article], 'amount':'0.000000001'}
+        transaction = {'name': name, 'article': [article], 'amount':'0.000000001'}
         try:
             Spicee.add_block(transaction)
             return True
@@ -74,19 +74,35 @@ class Article:
         
         for block in local_chain:
             block.print_contents()
+            print('\n')
 
     def fetch_article(self):
         user_hash = str(input('Search by block Hash: '))
+        result = False
         if len(user_hash) != 64:
             print("The Hash inputted does not seem to match the 64-bit format.")
         else:
             for block in Spicee.chain:
-                current_hash = block.hash
-                article_data = block.transactions
-            
-
-
-
+                if len(block.transactions) == 0:
+                    pass
+                else:
+                    data = block.transactions
+                    block_hash = block.hash
+                    if block_hash == user_hash:
+                        print(data)
+                        article = data[1]['article']
+                        name = data[0]['name']
+                        print('Name: {}'.format(name))
+                        print('\n')
+                        print(article)
+                        result = True
+                    else:
+                        continue
+                
+            if result == False:
+                print("Article not found")
+        
+        
 test = Article()
 print(test.running())
 
