a58c47b5d0415714e6fbab5ec3944936b21050df
Jaival Patel <jaival@Jaivals-MacBook-Air.local>
2022-02-25 09:07:14 -0500

    writing small messages

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 from blockchain import Blockchain
 import time
+from datetime import datetime
+from termcolor import colored
 
 Spicee = Blockchain()
 
@@ -13,7 +15,18 @@ class Article:
         
 
     def write_article(self):
-        pass 
+        current_date = datetime.now()
+        now_date = current_date.strftime("%m/%d/%Y, %H:%M:%S")
+        initial_intro = colored('Date Created: ', 'cyan')
+        print(colored('-'*24))
+        print('{}: {}'.format(initial_intro, now_date))
+        print('\n')
+        string = str(input('''-> '''))
+        print(string)
+        time.sleep(0.5)
+
+        
+        
 
     def add_article_to_chain(self):
         pass 
@@ -26,4 +39,6 @@ class Article:
             block.print_contents()
         
 
+test = Article()
+print(test.write_article())
 
